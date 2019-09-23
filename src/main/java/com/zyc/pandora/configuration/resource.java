d6daa8d2af63b31d0a19e1982448bf0a6385b16d
zyc <zhangyc@missfresh.cn>
2019-09-23 17:23:25 +0800

    静态资源映射

---
+++ src/main/java/com/zyc/pandora/configuration/resource.java
@@ -0,0 +1,24 @@
+package com.zyc.pandora.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author : zhangyuchen
+ * @date : 2019/9/23 17:21
+ */
+@Configuration
+public class resource  extends WebMvcConfigurerAdapter {
+
+    @Value("${file.basePath}")
+    private String basePath;
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(basePath).addResourceLocations(basePath);
+        super.addResourceHandlers(registry);
+    }
+}
